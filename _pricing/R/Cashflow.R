0bf4406b8cb9c16ffd9b7a2b9691bb3141d1fa13
Rishi D <68461494+print-hi@users.noreply.github.com>
2021-12-10 20:55:24 +1100

    changed var names

--- _pricing/R/Cashflow.R
+++ _pricing/R/Cashflow.R
@@ -2,10 +2,6 @@
 #'
 #' Simulate cash flows using Monte-Carlo methods for various policies
 #' @name cashflow
-#' @param age
-#' Initial age of policyholder in years
-#' @param gender
-#' Gender of policyholder, `gender = "F"` (female), `"M"` (male)
 #' @param policy
 #' Policy type to simulate:
 #' `policy = "AP"` (Account Based Pension),
@@ -14,40 +10,44 @@
 #' `"CA"` (Care Annuity),
 #' `"PA"` (Pooled Annuity),
 #' `"VA"` (Variable Annuity)
-#' @param paths
+#' @param age
+#' Initial age of policyholder in years
+#' @param sex
+#' sex of policyholder, `sex = "F"` (female), `"M"` (male)
+#' @param seed
+#' Seed for random generator
+#' @param n
 #' Number of paths to simulate (Monte-Carlo method)
 #' @return
 #' Matrix of cash flow vectors for each simulated path
 #' @export cashflow
 #' @examples
-#' cf <- cashflow(age = 65, gender = "M", policy = "VA", paths = 1000)
-cashflow <- function(age = 17, gender = "F", policy = "AP", paths = 1000) {
+#' cf <- cashflow(policy = "VA", age = 65, sex = "M", n = 1000)
+cashflow <- function(policy = "AP", age = 17, sex = "F", seed = 0, n = 1000) {
 
-    # Set state transition + cash flow functions based on input policy
-    if (policy == "RM") {
-        trans_gen <- get_health_state_3
-        cashf_gen <- cf_reverse_mortgage # nolint
-    } else if (policy == "CA") {
-        trans_gen <- get_health_state_5
-        cashf_gen <- cf_reverse_mortgage # nolint
-    } else {
-        trans_gen <- get_aggregate_mortality
-        cashf_gen <- switch(policy,
-                            "AP" = cf_account_based_pension,
-                            "LA" = cf_life_annuity,
-                            "CA" = cf_care_annuity,
-                            "PA" = cf_pooled_annuity,
-                            "VA" = cf_variable_annuity)
-    }
+    # Set cash flow functions based on input policy
+    cf_fn <- switch(policy,
+                    "AP" = cf_account_based_pension,
+                    "RM" = cf_reverse_mortgage,
+                    "VA" = cf_variable_annuity,
+                    "PA" = cf_pooled_annuity,
+                    "CA" = cf_care_annuity,
+                    "LA" = cf_life_annuity)
 
     # Get matrix of states for each path
-    state <- trans_gen(age, gender, paths)
+    if (policy == "RM") {
+        state <- get_health_state_3(age, sex, seed, n)
+    } else if (policy == "CA") {
+        state <- get_health_state_5(age, sex, seed, n)
+    } else {
+        state <- get_aggregate_mortality(age, sex, seed, n)
+    }
 
     # Get matrix of economic variables for each path
-    intrs <- get_interest()
-    infla <- get_inflation()
-    stock <- get_stock_price()
-    house <- get_house_price()
+    intrs <- get_interest(age, seed, n)
+    infla <- get_inflation(age, seed, n)
+    stock <- get_stock_price(age, seed, n)
+    house <- get_house_price(age, seed, n)
 
     # Organise economic inputs into a data.frame for each path
     econ <- list()
@@ -61,7 +61,7 @@ cashflow <- function(age = 17, gender = "F", policy = "AP", paths = 1000) {
     cf <- matrix(nrow = nrow(state), ncol = ncol(state))
 
     # Generate cash flows for each state vector
-    for (i in seq(1, nrow(state))) cf[i, ] <- cashf_gen(state[i, ], econ[[i]])
+    for (i in seq(1, nrow(state))) cf[i, ] <- cf_fn(state[i, ], econ[[i]])
 
     return(cf)
 }
@@ -70,7 +70,7 @@ cashflow <- function(age = 17, gender = "F", policy = "AP", paths = 1000) {
 ###### PLACEHOLDER FUNCTIONS
 
 # Temporary helper function, should link to health-state module
-get_health_state_3 <- function(age = 17, gender = "F", paths = 1000) {
+get_health_state_3 <- function(age = 17, sex = "F", seed = 0, n = 1000) {
   health_3 <- as.matrix(read.csv("R/data/health.csv"))
   health_3 <- ifelse(health_3 > 0, -2, health_3)
   colnames(health_3) <- NULL
@@ -79,7 +79,7 @@ get_health_state_3 <- function(age = 17, gender = "F", paths = 1000) {
 }
 
 # Temporary helper function, should link to health-state module
-get_health_state_5 <- function(age = 17, gender = "F", paths = 1000) {
+get_health_state_5 <- function(age = 17, sex = "F", seed = 0, n = 1000) {
   health_5 <- as.matrix(read.csv("R/data/health.csv"))
   colnames(health_5) <- NULL
   rownames(health_5) <- NULL
@@ -87,7 +87,7 @@ get_health_state_5 <- function(age = 17, gender = "F", paths = 1000) {
 }
 
 # Temporary helper function, should link to mortality module
-get_aggregate_mortality <- function(age = 17, gender = "F", paths = 1000) {
+get_aggregate_mortality <- function(age = 17, sex = "F", seed = 0, n = 1000) {
   mortality <- as.matrix(read.csv("R/data/mortality.csv"))
   colnames(mortality) <- NULL
   rownames(mortality) <- NULL
@@ -95,7 +95,7 @@ get_aggregate_mortality <- function(age = 17, gender = "F", paths = 1000) {
 }
 
 # Temporary helper function, should link to economic module
-get_interest <- function(age = 17, paths = 1000) {
+get_interest <- function(age = 17, seed = 0, n = 1000) {
   interest <- as.matrix(read.csv("R/data/interest.csv"))
   colnames(interest) <- NULL
   rownames(interest) <- NULL
@@ -103,7 +103,7 @@ get_interest <- function(age = 17, paths = 1000) {
 }
 
 # Temporary helper function, should link to economic module
-get_inflation <- function(age = 17, paths = 1000) {
+get_inflation <- function(age = 17, seed = 0, n = 1000) {
   inflation <- as.matrix(read.csv("R/data/inflation.csv"))
   colnames(inflation) <- NULL
   rownames(inflation) <- NULL
@@ -111,7 +111,7 @@ get_inflation <- function(age = 17, paths = 1000) {
 }
 
 # Temporary helper function, should link to economic module
-get_house_price <- function(age = 17, paths = 1000) {
+get_house_price <- function(age = 17, seed = 0, n = 1000) {
   house <- as.matrix(read.csv("R/data/house.csv"))
   colnames(house) <- NULL
   rownames(house) <- NULL
@@ -119,7 +119,7 @@ get_house_price <- function(age = 17, paths = 1000) {
 }
 
 # Temporary helper function, should link to economic module
-get_stock_price <- function(age = 17, paths = 1000) {
+get_stock_price <- function(age = 17, seed = 0, n = 1000) {
   stock <- as.matrix(read.csv("R/data/stock.csv"))
   colnames(stock) <- NULL
   rownames(stock) <- NULL
