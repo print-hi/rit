0dc2d7b2631d683bc4a2a3f7f29526edf060a435
YuxinZhou233 <YuxinZhou233@users.noreply.github.com>
2021-12-14 14:52:23 +1100

    create code for the 5 state model

---
+++ _health/code_5_state.R
@@ -0,0 +1,30 @@
+setwd("~/Documents/GitHub/retirement-toolkit/_health")
+install.packages("readxl")
+library("readxl")
+
+# read data
+my_data <- read_excel("Params_US_5_state.xlsx",col_names=TRUE)
+
+params_5_no_frailty <- my_data[1:3,3:ncol(my_data)] # model 3
+  
+params_5_no_frailty_with_trend <- my_data[5:8,3:ncol(my_data)] # model 4
+
+params_5_frailty <- my_data[10:14,3:ncol(my_data)] # model 5
+
+# select the parameter and model
+params=params_5_frailty
+model=5
+
+# input characteristics of the individual at time 0
+age=65
+gender=0
+i=9 # wave index
+latent=0 # initial value of latenr factor
+n=10000 # number of individuals
+
+# calculate transition probability
+transition_probability_5=function(params,age,gender,i,latent,n){
+  
+  
+}
+
