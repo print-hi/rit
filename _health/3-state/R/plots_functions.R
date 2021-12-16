fe1e74ab96c60e799cdb021efa03a5bdd39cded2
Alex Xiao <axiao124@gmail.com>
2021-12-16 16:55:15 +1100

    Added function that generates plot of surviving to each different state (or just alive in general).

--- _health/3-state/R/plots_functions.R
+++ _health/3-state/R/plots_functions.R
@@ -1,4 +1,6 @@
 # Functions to create different plots
+# used tidyverse
+
 surv_prob <- function(init_state, init_age, target_age, trans_probs, end_state = NULL) {
   # screening for errors
   if (init_state != 1 & init_state != 2) {
@@ -35,7 +37,45 @@ surv_prob <- function(init_state, init_age, target_age, trans_probs, end_state =
 }
 
 
+prob_plots <- function(init_state, init_age, trans_probs) {
+  # create 3 different probabilities:
+  # general alive probability, healthy, and disabled
+  alive_probs <- c(1)
 
+  if (init_state == 1) {
+    healthy_probs <- c(1)
+    disabled_probs <- c(0)
+  } else {
+    healthy_probs <- c(0)
+    disabled_probs <- c(1)
+  }
+  for (target in (init_age+1):110) {
+    alive_probs <- append(alive_probs, surv_prob(init_state, init_age, target, trans_probs))
+    healthy_probs <- append(healthy_probs, surv_prob(init_state, init_age, target, trans_probs, end_state = 1))
+    disabled_probs <- append(disabled_probs, surv_prob(init_state, init_age, target, trans_probs, end_state = 2))
+  }
+
+  # create age axis
+  ages <- init_age:110
+
+  # create dataframe for ggplot
+  survival_df <- data.frame('age' = ages,
+                              'alive' = alive_probs,
+                              'healthy' = healthy_probs,
+                              'disabled' = disabled_probs)
+
+  updated_df <- survival_df %>%
+    select(age, alive, healthy, disabled) %>%
+    tidyr::gather(key = 'Type', value = 'value', -age)
+
+  surv_plot <- ggplot(updated_df, aes(x = age, y = value)) +
+    geom_line(aes(color = Type)) +
+    scale_color_manual(labels = c('Alive', 'Disabled', 'Healthy'),
+                       values = c('darkolivegreen2', 'lightcoral', 'skyblue1')) +
+    title(main = 'Probability of Surviving to each Different State')
+
+  return(surv_plot)
+}
 
 
 
