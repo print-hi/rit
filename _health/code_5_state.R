9fb69cf28d6ece8e1d781ab272c56fb82ade3457
YuxinZhou233 <YuxinZhou233@users.noreply.github.com>
2021-12-20 14:25:45 +1100

    Update on calculating the transition rate, transition probability, life table, and simulation for the 5-state frailty model.

--- _health/code_5_state.R
+++ _health/code_5_state.R
@@ -1,6 +1,6 @@
 setwd("~/Documents/GitHub/retirement-toolkit/_health")
-install.packages("readxl")
 library("readxl")
+library("expm")
 
 # read data
 my_data <- read_excel("Params_US_5_state.xlsx",col_names=TRUE)
@@ -20,11 +20,82 @@ age=65
 gender=0
 i=9 # wave index
 latent=0 # initial value of latenr factor
-n=10000 # number of individuals
 
-# calculate transition probability
-transition_probability_5=function(params,age,gender,i,latent,n){
-  
-  
+# function to calculate transition rate
+transition_rate_5_frailty=function(params,age,gender,i,latent){
+  vari=matrix(c(1,age,gender,i,latent),ncol=1) # construct a column vector of the variables
+  ln_trans_rate=t(params)%*%vari # do matrix calculation to get a column vector of the ln transition rates
+  trans_rate=exp(ln_trans_rate) # column vector of transition rates
+  return(trans_rate)
 }
+
+### test package expm
+# abc=matrix(c(-0.7,0.2,0.5,0.3,-0.3,0.3,0.4,0.1,-0.8),ncol=3)
+abc=rbind(c(-0.7,0.3,0.4),c(0.2,-0.3,0.1),c(0.5,0.3,-0.8))
+expm(abc)
+
+# function to build transition probability matrix
+transition_probability_5_frailty=function(trans_rate){
+  trans_rate_matrix=rbind(c(-sum(trans_rate[1:4]),trans_rate[1],trans_rate[2],trans_rate[3],trans_rate[4]),
+                          c(0,-sum(trans_rate[5:6]),0,trans_rate[5],trans_rate[6]),
+                          c(trans_rate[7],trans_rate[8],-sum(trans_rate[c(7,8,9,10)]),trans_rate[9],trans_rate[10]),
+                          c(0,trans_rate[11],0,-sum(trans_rate[11:12]),trans_rate[12]),
+                          c(0,0,0,0,0))
+  trans_prob_matrix=expm(trans_rate_matrix)
+  
+  trans_prob_matrix
+}
+
+# simulation section
+gender=0
+i=9 # wave index
+latent=0 # initial value of latenr factor
+n=1000 # number of simulations
+
+state_status_full=list() # full list of state status for all n simulations
+
+for (n in 1:1000){ # 1000 simulations
+  # list of 46 vectors of transition rates for this simulation
+  trans_rate=list()
+  # list of 46 matrics of transition probabilities for this simulation
+  trans_prob_matrix=list()
+  # list of 46 matrics of accumulated transition probabilities for this simulation
+  trans_sum_prob_matrix=list()
+  #list of 
+  state_status=list()
+  state_status[[1]]=cbind(c(1,0,0,0,0)) # initial state status is 1 in the healthy state and 0 for the others
+  for (age in 65:110){
+    state_status[[age-63]]=cbind(c(0,0,0,0,0)) # construct the list to be filled later
+  }
+for (age in 65:110){
+  trans_rate[[age-64]]=transition_rate_5_frailty(params,age,gender,i,latent) # calculate transition rate for each age
+  
+  trans_prob_matrix[[age-64]]=transition_probability_5_frailty(trans_rate[[age-64]]) # calculate transition probability matrix for each age
+  
+  if (age > 65) {
+  trans_sum_prob_matrix[[age-64]]=trans_sum_prob_matrix[[age-65]]%*%trans_prob_matrix[[age-64]] # calculate accumulated transition probability matrix up to each age
+  }else{
+  trans_sum_prob_matrix[[age-64]]=trans_prob_matrix[[age-64]] 
+  }
+  
+  # calculate the probability to be at state j after 1 year
+  for (j in 1:5){
+  state_status[[age-63]][j,1]=state_status[[age-64]][1,1]*trans_prob_matrix[[age-64]][1,j]+ 
+                              state_status[[age-64]][2,1]*trans_prob_matrix[[age-64]][2,j]+
+                              state_status[[age-64]][3,1]*trans_prob_matrix[[age-64]][3,j]+
+                              state_status[[age-64]][4,1]*trans_prob_matrix[[age-64]][4,j]+
+                              state_status[[age-64]][5,1]*trans_prob_matrix[[age-64]][5,j]
+  # the probability/or number(if assume 10000 individuals just simply times 10000) of the state at the beginning of this year times the probability of entering state j from this state
+  # then take the sum of all possible initial states of this year
+  }
+  
+  latent=latent+rnorm(1) # simulate the latent factor
+}
+  state_status_full=append(state_status_full,list(state_status)) # state_status_full collects all state status at all ages and of all simulations
+  # the size of this list will be large
+}
+
+
+
+
 
