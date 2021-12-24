a2624c12dcdcac436a3b06f3a06a701d8e974818
Alex Xiao <axiao124@gmail.com>
2021-12-24 14:25:06 +1100

    Changed afl and afld function to use probability instead of life table and simulation

--- _health/3-state/R/statistics_functions.R
+++ _health/3-state/R/statistics_functions.R
@@ -1,82 +1,32 @@
 # statistics functions
 
-#' average future lifetime
-#'
-#' Calculates average future lifetime remaining from initial age and state. Note that the
-#' average future lifetime calculates the remaining time left (regardless of state) from the
-#' initial state. So remaining lifetimes for individuals starting healthy or disabled will differ.
-#'
-#' @param life_table
-#' This is a life table generated from \code{\link[tshm]{create_life_table}}.
-#' Since the creation of the life table already has an initial state that the cohort
-#' is in, the function does not require the same input.
-#'
-#' @return
-#' Numeric output for the average future lifetime. This is calculated by taking the
-#' curtate expected lifetime; essentially a sum of survival probabilities.
-#'
-#' @export
-#'
-#' @examples
-afl <- function(life_table) {# average future life time
-  # we will sum up over all the survival probabilities
-  average_life <- 0
-  for (i in 2:nrow(life_table)) {
-    average_life = average_life + life_table[i, 'Lx']/life_table[1, 'Lx']
+
+
+afl <- function(init_age, init_state, trans_probs) {
+  probs <- c()
+  for (i in 1:(110-init_age)) {
+    prob <- tshm::surv_prob(init_state, init_age, init_age+i, trans_probs)
+    probs <- append(probs, prob)
   }
-  return(average_life)
+  return(sum(probs))
 }
 
-#' Average future lifetime in disabled state
-#'
-#' Calculates the average future lifetime that will be spent in disabled state from
-#' initial age and state. This average future lifetime depends on the initial state
-#' the individual is in. Generally, this is applied to non-homogeneus Markov chains,
-#' so we will use stochastic simulation to calculate this statistic.
-#' NOTE: THIS FUNCTION SHOULD ONLY BE USED WITH STATIC AND TREND MODEL, USE ....
-#' FOR FRAILTY MODEL.
-#'
-#' @param init_age
-#' Integer between 65 and 110 specifying the initial age of the individual
-#' @param init_state
-#' 0 for healthy, 1 for disabled
-#' @param trans_probs
-#' list of transition probability matrices, preferably generated from \code{\link[tshm]{get_trans_probs}}.
-#' @param n
-#' integer denoting number of individuals in simulation
-#'
-#' @return
-#' numeric output for average time spent in disabled state
-#'
-#' @export
-#'
-#' @examples
-afld <- function(init_age, init_state, trans_probs, n = 500000) { # average future lifetime disabled
-  # we do this by simulating stochastic lifetimes
-  # flagging errors
-  if (init_age < 65 | init_age > 110) {
-    return('Error: Please enter an age between 65 and 110.')
+
+
+afld <- function(init_age, init_state, trans_probs) {
+  # we sum up probabilities of being disabled in each state
+  # this is similar to curtate life expectation
+  probs <- c()
+  for (i in 1:(110-init_age)) {
+    prob <- tshm::surv_prob(init_state, init_age, init_age+i, trans_probs, end_state = 1)
+    probs <- append(probs, prob)
   }
-
-  if (init_state != 0 & init_state != 1) {
-    return('Error: please input 0 (healthy) or 1 (disabled) for initial state.')
-  }
-
-  if (n != as.integer(n)) {
-    return('Error: Please input an integer for n.')
-  }
-
-  simulated_path <- simulate_path(init_age, init_state, trans_probs, cohort = n)
-  # count disabled times
-  disabled_time <- sum(simulated_path == 1)
-
-  if (init_state == 1) {
-    return(disabled_time/n-0.5) # we count extra 0.5 years before the initial age that we need to take off
+  if (init_state == 0) {
+    return(sum(probs))
   } else {
-    return(disabled_time/n)
+    return(sum(probs) + 0.5) # extra half year of disabled at the start
   }
 }
-
 
 #' Average future lifetime in disabled state (frailty moded)
 #'
