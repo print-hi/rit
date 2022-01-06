56f2b1972f6f9e5001d390c85720ddce57c42bd5
TingLi7 <tiantingli7@gmail.com>
2022-01-06 19:58:43 +1100

    initial draft of P to Q function

---
+++ _mortality/R/price_of_risk.R
@@ -0,0 +1,124 @@
+
+#' Convert death probabilities to survival rates
+#'
+#' Converts 1-year death probabilities to the associated survival function.
+#' @param qx
+#' vector, matrix or 3D array of 1-year death probabilities with age
+#' (on the rows) and calendar year (on the columns)
+#'
+#' @return
+#' survival rates in the same format as `qx`
+#'
+#' @export
+#'
+#' @examples
+#'
+q2survival <- function(qx) {
+  px <- 1 - qx
+
+  if (length(dim(px)) == 2) {
+    return(apply(px, 2, cumprod))
+  } else if (length(dim(px)) == 3) {
+    return(arr_apply(px, function(x) apply(x, 2, cumprod)))
+  }
+
+  # Dealing with vector, need to check/improve
+  return(cumprod(px))
+}
+
+
+#' Survival Function Transformation
+#'
+#' Transforms the survival function from the real world P-measure to the risk-neutral
+#' Q-measure according to the specified distortion risk measure
+#'
+#' The distortion risk measures and their strings are as follows
+#' * "wang": Wang Transform
+#' * "ph": Proportional Hazard Transform
+#' * "dp": Dual-power Transform
+#' * "dadp": Denneberg's Absolute Deviation Principle
+#' * "exp": Exponential Transform
+#' * "log": Logarithmic Transform
+#'
+#' @param StP
+#' vector, matrix or 3D array of the survival function under the P-measure with
+#' survival time (on the rows) and calendar year (on the columns)
+#' @param method
+#' character string representing the distortion risk measure to be used. See "Details".
+#' @param lambda
+#' parameter associated with the distortion risk measure
+#' @param t
+#' optional vector of survival time
+#' @param ages
+#' optional vector of ages
+#' @param years
+#' optional vector of years
+#'
+#' @return
+#' the transformed survival function under the Q-measure
+#' @export
+#'
+#' @examples
+#'
+survivalP2Q <- function(StP, method, lambda, t = NULL, ages = NULL, years = NULL) {
+
+  # Need to make sure values only between 0 and 1
+
+  # Defining distortion functions
+  wang <- function(x, lam) {
+    if (lam < 0) stop("invalid lambda value")
+    return(1 - pnorm(qnorm(1 - x) - lam))
+  }
+
+  ph <- function(x, lam) {
+    if (lam < 1) stop("invalid lambda value")
+    return(x^(1 / lam))
+  }
+
+  dp <- function(x, lam) {
+    if (lam < 1) stop("invalid lambda value")
+    return(1 - (1 - x)^lam)
+  }
+
+  gp <- function(x, lam) {
+    if (lam < 0 | lam > 1) stop("invalid lambda value")
+    return((1 + lam) * x - lam * x^2)
+  }
+
+  dadp <- function(x, lam) {
+    if (lam < 0 | lam > 1) stop("invalid lambda value")
+    return(ifelse(x < 0.5, (1 + lam) * x, lam + (1 - lam) * x))
+  }
+
+  exp_tfm <- function(x, lam) {
+    if (lam <= 0) stop("invalid lambda value")
+    return((1 - exp(-lam * x)) / (1 - exp(-lam)))
+  }
+
+  log_tfm <- function(x, lam) {
+    if (lam <= 0) stop("invalid lambda value")
+    return(log(1 + lam * x) / log(1 + lam))
+  }
+
+  type <- c("wang", "ph", "dp", "gp", "dadp", "exp", "log")
+
+  if (!is.element(method, type)) stop("invalid method type")
+  else if (method == "wang") distort <- wang
+  else if (method == "ph") distort <- ph
+  else if (method == "dp") distort <- dp
+  else if (method == "gp") distort <- gp
+  else if (method == "dadp") distort <- dadp
+  else if (method == "exp") distort <- exp_tfm
+  else if (method == "log") distort <- log_tfm
+
+  distort(StP, lambda)
+
+}
+
+pdfP2Q <- function(StP, method, lambda, t = NULL, ages = NULL, years = NULL) {
+
+  type <- c("canon", "esscher")
+
+  if (!is.element(method, type)) stop("invalid input for conversion")
+
+}
