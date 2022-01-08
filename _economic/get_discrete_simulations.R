f02f22ad5a8ef1836918bb1a0c5cf1449859259c
Queenie-Huang <92086843+Queenie-Huang@users.noreply.github.com>
2022-01-08 15:55:22 +1100

    Function to generate VAR simulations

--- _economic/get_discrete_simulations.R
+++ _economic/get_discrete_simulations.R
@@ -1,23 +1,27 @@
 #' get_discrete_simulations
 #'
-#' Returns the simulated paths for various economic and financial variables on a quarterly basis: (1) Australia 3-month zero-coupon yields, (2) Australia 10-year zero-coupon spread, (3) New South Wales houses value index, (4) New South Wales houses rental yields, (5) Australian GDP, (6) Australian CPI, (7) S&P/ASX200 closing price, (8) Australian dollar trade-weighted index, (9) Australia mortgage rate, (10) New South Wales unemployment rate. 
-#' Simulations are based on a Vector Autoregression model. It is recommended to simulate at most 10 years. 
+#' Returns the simulated paths for various economic and financial variables: (1) Australia 3-month zero-coupon yields, (2) Australia 10-year zero-coupon spread, (3) New South Wales houses value index, (4) New South Wales houses rental yields, (5) Australian GDP, (6) Australian CPI, (7) S&P/ASX200 closing price, (8) Australian dollar trade-weighted index, (9) Australia mortgage rate, (10) New South Wales unemployment rate. 
+#' Simulations are based on a Vector Autoregression model. 
 #' 
-#' @param num_years Number of years for forecasts, counting from 2021 Quarter 2. Default is 5 years. 
+#' @param num_years Number of years to forecast, counting from 2021-01-01. Default is 5 years, recommended period is less than 10 years. 
 #' @param num_paths Number of simulation paths. Default is 10000 paths. 
+#' @param frequency One of "year", "quarter", and "month". Default is "quarter", which is the simulation frequency for the Vector Autoregression model. Linear interpolation will be used if the required frequency is higher, whereas arithmetic average will be used if the frequency is lower.   
 #'
-#' @return Simulation trajectories. 
+#' @return A list containing 10 data frames for the simulated trajectories for each economic variable. 
 #' @export
 #'
-#' @examples get_discrete_simulations(num_years = 10, num_paths = 100)
+#' @examples get_discrete_simulations(num_years = 10, num_paths = 10000, frequency = "year")
 #' 
-get_discrete_simulations = function (num_years = 5, num_paths = 10000) {
+get_discrete_simulations = function (num_years = 5, num_paths = 10000, frequency = "quarter") {
     
     ################
     # error messages 
-    is.wholenumber <- function(x, tol = .Machine$double.eps^0.5)  abs(x - round(x)) < tol
+    is.wholenumber = function(x, tol = .Machine$double.eps^0.5)  abs(x - round(x)) < tol
     if (num_years <= 0 | num_paths <= 0 | !is.wholenumber(num_years) | !is.wholenumber(num_paths)) {
         stop("Number of years and paths to simulate must be positivie integers. ")
+        
+    } else if (!frequency %in% c("year", "quarter", "month")) { 
+        stop ("Frequency must be one of 'year', 'quarter', and 'month'. ")
         
     } else {
         ########################################################
@@ -53,9 +57,9 @@ get_discrete_simulations = function (num_years = 5, num_paths = 10000) {
         ###########################
         # simulation initialisation
         # starting quarter 
-        init_qtr = as.Date("2021-04-01")
+        init_qtr = as.Date("2021-01-01")
         num_pred = 4 * num_years 
-        time_index = as.yearqtr(seq(from = init_qtr, length.out = num_pred, by = "quarter"))
+        time_index = seq(from = init_qtr, length.out = num_pred + 1, by = "quarter")
         path_index = paste("trajectory_", 1:num_paths, sep = "")
         # initial values for stationary series 
         init_stat = c(-0.033243728,1.422580645,0.050236685, -0.000455718,0.018766511,0.005954930,0.059250269,0.037258168)
@@ -66,22 +70,17 @@ get_discrete_simulations = function (num_years = 5, num_paths = 10000) {
         # step-by-step simulations 
         # white noise
         noise = matrix(data = rnorm(length(intercept) * num_pred * num_paths, 0, 1), nrow = length(intercept))
-        # one-step (inputs/outputs are both stationary)
-        var_step = function (initial, noise_index) {
-            e = as.vector(noise[,noise_index])
-            return (intercept + coef %*% initial + as.matrix(chol(covres)) %*% e)
-        }
-        
         # whole path (inputs/outputs are both stationary)
         var_path = function (num_pred, noise_index) {
             path = as.data.frame(matrix(NA, nrow = num_pred, ncol = length(var_names)))
-            row.names(path) = time_index
+            row.names(path) = time_index[-1]
             colnames(path) = var_names 
             
             # simulate for num_pred steps 
             new_init = init_stat
             for (i in 1:num_pred) {
-                new = var_step(new_init, noise_index[i])
+                e = as.vector(noise[,noise_index[i]])
+                new = intercept + coef %*% new_init + as.matrix(chol(covres)) %*% e
                 path[i,] = new
                 new_init = new
             }
@@ -95,8 +94,6 @@ get_discrete_simulations = function (num_years = 5, num_paths = 10000) {
             # loops thru the series (separate lists)
             v_path = list()
             noise_index = 1:num_pred
-            #v_path = lapply(v_path, function (x) {var_path(num_pred, noise_index + num_pred * x)})
-            
             for (path in 1:num_paths) {
                 v_path[[path]] = var_path(num_pred, noise_index)
                 noise_index = noise_index + num_pred
@@ -118,7 +115,7 @@ get_discrete_simulations = function (num_years = 5, num_paths = 10000) {
             stat$AUD = lapply(v_path, function (x) {x[,8]})
             
             stat = lapply(stat, function(x){x = as.data.frame(x)})
-            stat = lapply(stat, function(x){row.names(x) = time_index; x})
+            stat = lapply(stat, function(x){row.names(x) = time_index[-1]; x})
             stat = lapply(stat, function(x){colnames(x) = path_index; x})
             return (stat)
         }
@@ -128,28 +125,58 @@ get_discrete_simulations = function (num_years = 5, num_paths = 10000) {
         # convert forecasted variables -> original units 
         diff_inv = function (x, init) {
             # undo differencing once: zcp3m_yield, rental_yield
-            diffinv(x, xi = init)[-1]
+            diffinv(x, xi = init)
         }
         index2grow_inv = function (x, init) {
-            # 1 reverse index to growth rate 
-            # 2 reverse shortening once: home_value, gdp, cpi, asx200, aud
-            Reduce (function (init, x) {init * exp(x)}, c(init, x), accumulate = T)[-1]
+            # reverse index to growth rate: home_value, gdp, cpi, asx200, aud
+            Reduce (function (init, x) {init * exp(x)}, c(init, x), accumulate = T)
         }
         
         # adj for the original series
-        output = list ()
-        output[[1]] = apply(stat[[1]], 2, function (x) {diff_inv(x, init_orig[1])}) # zcp3m_yield 
-        output[[2]] = stat[[2]] # zcp10y_spread: not changed 
-        output[[3]] = apply(stat[[3]], 2, function (x) {index2grow_inv(x, init_orig[3])}) # home_index
-        output[[4]] = apply(stat[[4]], 2, function (x) {diff_inv(x, init_orig[4])}) # rental_yield 
-        output[[5]] = apply(stat[[5]], 2, function (x) {index2grow_inv(x, init_orig[5])}) # GDP
-        output[[6]] = apply(stat[[6]], 2, function (x) {index2grow_inv(x, init_orig[6])}) # CPI
-        output[[7]] = apply(stat[[7]], 2, function (x) {index2grow_inv(x, init_orig[7])}) # ASX200
-        output[[8]] = apply(stat[[8]], 2, function (x) {index2grow_inv(x, init_orig[8])}) # AUD
-        output[[9]] = output[[1]] + 2.825 # mortage_rate
-        output[[10]] = output[[2]] + 4.956 # unemployment_rate 
+        sim = list ()
+        sim[[1]] = apply(stat[[1]], 2, function (x) {diff_inv(x, init_orig[1])}) # zcp3m_yield 
+        sim[[2]] = rbind(init_orig[2], stat[[2]]) # zcp10y_spread: not changed 
+        sim[[3]] = apply(stat[[3]], 2, function (x) {index2grow_inv(x, init_orig[3])}) # home_index
+        sim[[4]] = apply(stat[[4]], 2, function (x) {diff_inv(x, init_orig[4])}) # rental_yield 
+        sim[[5]] = apply(stat[[5]], 2, function (x) {index2grow_inv(x, init_orig[5])}) # GDP
+        sim[[6]] = apply(stat[[6]], 2, function (x) {index2grow_inv(x, init_orig[6])}) # CPI
+        sim[[7]] = apply(stat[[7]], 2, function (x) {index2grow_inv(x, init_orig[7])}) # ASX200
+        sim[[8]] = apply(stat[[8]], 2, function (x) {index2grow_inv(x, init_orig[8])}) # AUD
+        sim[[9]] = sim[[1]] + 2.825 # mortage_rate
+        sim[[10]] = sim[[2]] + 4.956 # unemployment_rate 
+        sim = lapply(sim, function (x) {x = as.data.frame(x)})
+        sim = lapply(sim, function (x) {row.names(x) = time_index; x})
+        names(sim) = sim_var_names
+        rm(stat, noise)
+        
+        ###############
+        # Adj frequency
+        output = list()
+        if (frequency == "month") {
+            time_index_month = seq(from = init_qtr, length.out = num_years * 12 + 1, by = "month")
+            
+            qtr2month = function (x) {
+                # transforms quarterly data to monthly data 
+                qtr_data = zoo(x, time_index)
+                month_data = zoo (NA, time_index_month)
+                data = merge (qtr_data, month_data)
+                data$month_data = na.approx(data$qtr_data, rule=12)
+                return (as.vector(data$month_data))
+            }
+            output = lapply(sim, function (x) apply(x, 2, qtr2month))
+            output = lapply(output, function(x) {row.names(x) = as.character(time_index_month); x})
+            output = lapply(output, function(x) {x = x[-nrow(x), ]}) # remove the last row (Jan 01)
+            
+        } else if (frequency == "quarter") {
+            output = lapply(sim, function(x) {x = x[-nrow(x), ]}) # remove the last row (Jan 01)
+            
+        } else if (frequency == "year") {
+            time_index_year = seq(from = init_qtr, length.out = num_years, by = "year")
+            output = lapply(sim, function (x) apply(x[-nrow(x), ], 2, function (y) {colMeans(matrix(y, nrow=4))} ))
+            output = lapply(output, function(x) {row.names(x) = as.character(time_index_year); x})
+        }
+        
         output = lapply(output, function(x){x = as.data.frame(x)})
-        names(output) = sim_var_names
         
         return (output)
     }
