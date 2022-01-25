5a86fc64afb0e5f16cf9c792a5c21249c12f026f
Alex Xiao <axiao124@gmail.com>
2022-01-25 16:35:48 +1100

    added china CLHLS parameters + documentation

--- _health/3-state/R/data.R
+++ _health/3-state/R/data.R
@@ -1,6 +1,6 @@
 #' Parameters of Cox Regression Model estimated from US HRS Study
 #'
-#' A dataset containing the parametets of the static, trend and frailty cox regression
+#' A dataset containing the parameters of the static, trend and frailty cox regression
 #' models estimated in the 1998-2014 US Health and Retirement Study. The model includes
 #' parameters for the following covariates: age, gender, time (trend), stochastic latent factor
 #' (frailty).
@@ -17,3 +17,24 @@
 #'
 #' @source \url{https://cepar.edu.au/publications/working-papers/functional-disability-systematic-trends-and-uncertainty-comparison-between-china-and-us}
 'US_HRS'
+
+
+#' Parameters of Cox Regression Model estimated from China CLHLS Study
+#'
+#' A dataset containing the parameters of the static, trend and frailty cox regression
+#' models estimated in the 1998-2014 Chinese Longitudinal Healthy Longevity Survey (CLHLS). The
+#' model includes parameters for the following covariates: age, gender, time (trend) stochastic
+#' latent factor (frailty).
+#'
+#' @format A tibble OR data frame with 15 rows and 5 columns:
+#' \describe{
+#'    \item{Model}{static, trend or frailty}
+#'    \item{Coef}{the covariate that the coefficient is corresponding to}
+#'    \item{trans1}{parameter estimate for H to F transition}
+#'    \item{trans2}{parameter estimate for F to H transition}
+#'    \item{trans3}{parameter estimate for H to D transition}
+#'    \item{trans4}{parameter estimate for F to D transition}
+#' }
+#'
+#' @source \url{https://cepar.edu.au/publications/working-papers/functional-disability-systematic-trends-and-uncertainty-comparison-between-china-and-us}
+'china_CLHLS'
