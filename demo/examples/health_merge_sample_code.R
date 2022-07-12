2ae4978e0015aa0247ff4d3b84f58d6d4e8bb4bc
YuxinZhou233 <YuxinZhou233@users.noreply.github.com>
2022-07-12 20:55:25 +1000

    update sample code

--- demo/examples/health_merge_sample_code.R
+++ demo/examples/health_merge_sample_code.R
@@ -15,7 +15,8 @@ trans_probs_5state=get_trans_probs(n_states, model_type, param_file, init_age, f
 
 lifetable_created_5state=create_life_table(model_type, trans_probs=trans_probs_5state, init_age, init_state = 0, cohort = 1)
 
-lifetable_simulated_5state=simulate_life_table(model_type, param_file, female,year = 2012,wave_index = 8,latent=0,init_age,init_state=0,n_sim=100, mean=FALSE)
+lifetable_simulated_5state=simulate_life_table(model_type, param_file, female,year = 2012,wave_index = 8,latent = 0,init_age,init_state = 0,n_sim = 100,cohort = 1,mean = FALSE)
+mean_lifetable_simulated_5state=simulate_life_table(model_type, param_file, female,year = 2012,wave_index = 8,latent = 0,init_age,init_state = 0,n_sim = 100,cohort = 1,mean = TRUE)
 
 simulated_paths_5state=simulate_health_state_paths(trans_probs=trans_probs_5state, init_age, init_state = 0, cohort = 100000)
 ###
