07b5fc40c935d26adcd441afbd3f18f5026b0d4c
Alex Xiao <axiao124@gmail.com>
2022-07-27 14:39:46 +1000

    fixed export documentation

--- R/health3_Plot.R
+++ R/health3_Plot.R
@@ -19,7 +19,7 @@
 #' @return
 #' numeric denoting probability of surviving to end age in end state
 #'
-#' @export surv_prob
+#' @export
 #'
 #' @examples example
 health3_surv_prob <- function(init_state, init_age, target_age, trans_probs, end_state = NULL) {
@@ -86,7 +86,7 @@ health3_surv_prob <- function(init_state, init_age, target_age, trans_probs, end
 #' line plot with 3 lines, each denoting survival probability, survivial probability to
 #' disabled, and survival probability to healthy.
 #'
-#' @export prob_plots
+#' @export
 #'
 #' @import tidyr dplyr ggplot2
 #'
